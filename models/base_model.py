fae2288b611f901d96f7c262826723e514874fe4
Samuel Ibanga <ibangajnr27@gmail.com>
2023-02-07 11:30:02 +0100

    basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,13 @@
 """Defines the BaseModel class"""
 
 
-from uuid import UUID, uuid4
+from uuid import uuid4
 from datetime import datetime
 
 class BaseModel():
     """BaseModel class defines all common attributes/methods for other classes of the HBnB project."""
 
-    def __init__(self, id=uuid4(), created_at=datetime.now(), updated_at=datetime.now()):
+    def __init__(self, *args, **kwargs):
         """__init__ class method initializes instance of the BaseModel class
 
         Args:
@@ -16,16 +16,22 @@ class BaseModel():
             created_at (datetime): Assign with the current datetime when an instance is created.
             updated_at (datetime): Assign with the current datetime when an instance is created and it will be updated everytime project is changed.
         """
-        self.id = str(id)
-        self.created_at = created_at
-        self.updated_at = updated_at
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """__str__ method that prints a string representation of BaseModel class
         [<class name>] (<self.id>) <self.__dict__>
         """
         print("[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__))
-
 
     def save(self):
         """save method updates the public instance attribute updated_at with the current datetime"""
@@ -40,3 +46,6 @@ class BaseModel():
         base_model_dict["id"] = self.id
 
         return base_model_dict
+
+
+bm = BaseModel()
