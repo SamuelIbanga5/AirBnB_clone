db0e5cbb85e5b6f3ea8e052d4b4321483a5c0f73
Samuel Ibanga <ibangajnr27@gmail.com>
2023-02-08 04:08:06 +0100

    Updated

--- tests/test_save_reload_base_model.py
+++ tests/test_save_reload_base_model.py
@@ -2,7 +2,7 @@
 from models import storage
 from models.base_model import BaseModel
 
-all_objs = models.storage.all()
+all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
