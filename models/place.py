8457dc3263235b8db95e6f4661eb5ac2408fc7c0
Samuel Ibanga <ibangajnr27@gmail.com>
2023-02-11 16:11:32 +0100

    Place class

---
+++ models/place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines Place class inherits from BaseModel"""
+
+
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """Defines Place class inheriting attributes from BaseModel"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = ""
+    number_bathrooms = ""
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
